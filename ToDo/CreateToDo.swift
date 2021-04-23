68bbd17070b5d72c6c4fb2c9f5c48e78277ec472
Waihon Yew <yewwaihon@gmail.com>
2021-04-23 14:42:59 +0800

    Re-indent the new CreateToDo to preferred 2 spaces

--- ToDo/CreateToDo.swift
+++ ToDo/CreateToDo.swift
@@ -8,13 +8,13 @@
 import SwiftUI
 
 struct CreateToDo: View {
-    var body: some View {
-        Text("Create To Do view!")
-    }
+  var body: some View {
+    Text("Create To Do view!")
+  }
 }
 
 struct CreateToDo_Previews: PreviewProvider {
-    static var previews: some View {
-        CreateToDo()
-    }
+  static var previews: some View {
+    CreateToDo()
+  }
 }
