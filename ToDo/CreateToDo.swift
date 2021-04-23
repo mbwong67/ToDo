778798d5b3fe3a2ce51d6b9d215adb4ed75bc358
Waihon Yew <yewwaihon@gmail.com>
2021-04-23 14:33:19 +0800

    Add a CreateToDo view to be called by To Dos list

---
+++ ToDo/CreateToDo.swift
@@ -0,0 +1,20 @@
+//
+//  CreateToDo.swift
+//  ToDo
+//
+//  Created by Yew Wai Hon on 23/04/2021.
+//
+
+import SwiftUI
+
+struct CreateToDo: View {
+    var body: some View {
+        Text("Create To Do view!")
+    }
+}
+
+struct CreateToDo_Previews: PreviewProvider {
+    static var previews: some View {
+        CreateToDo()
+    }
+}
